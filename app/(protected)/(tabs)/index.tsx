d8e1a2dfd79b38069a93da5e5900cfa9d38442aa
brunolima <francisco.bruno@grupomateus.com>
2026-09-13 17:36:45 -0300

    feat: criando o componente de icon, adicionando fontes

--- app/(protected)/(tabs)/index.tsx
+++ app/(protected)/(tabs)/index.tsx
@@ -1,4 +1,5 @@
 import { CityCard } from "@/src/components/CityCard";
+import { Icon } from "@/src/components/Icon";
 import { Screen } from "@/src/components/Screen";
 import { cityPreviewList } from "@/src/data/cities";
 import { CityPreview } from "@/src/types";
@@ -12,6 +13,8 @@ export default function HomeScreen() {
   }
   return (
     <Screen>
+      <Icon name="Adventure" color="primary" />
+      <Icon name="Beach" color="primary" />
       <FlatList data={cityPreviewList} renderItem={renderItem} />
     </Screen>
   );
