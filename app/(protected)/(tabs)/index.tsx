061ec58f67487af73e53b3e71fb6db80eb8bd928
brunolima <francisco.bruno@grupomateus.com>
2026-09-13 17:05:39 -0300

    feat: criando o componente Screen, Box e Text

--- app/(protected)/(tabs)/index.tsx
+++ app/(protected)/(tabs)/index.tsx
@@ -1,15 +1,18 @@
-import { Box } from "@/src/components/Box";
-import { Text } from "@/src/components/Text";
-import { useAppTheme } from "@/src/theme/useAppTheme";
+import { CityCard } from "@/src/components/CityCard";
+import { Screen } from "@/src/components/Screen";
+import { cityPreviewList } from "@/src/data/cities";
+import { CityPreview } from "@/src/types";
+import { FlatList, ListRenderItemInfo } from "react-native";
 
 export default function HomeScreen() {
-  const { colors } = useAppTheme();
+  // const { colors } = useAppTheme();
 
+  function renderItem({ item }: ListRenderItemInfo<CityPreview>) {
+    return <CityCard cityPreview={item} />;
+  }
   return (
-    <Box flex={1} backgroundColor="midnightBlack">
-      <Text mt="s10" color="text">
-        HomeScreen {colors.midnightBlack}
-      </Text>
-    </Box>
+    <Screen>
+      <FlatList data={cityPreviewList} renderItem={renderItem} />
+    </Screen>
   );
 }
