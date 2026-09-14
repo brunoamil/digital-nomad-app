77a1a61437f5dbe03566785cc1deb871306fb6d1
brunolima <francisco.bruno@grupomateus.com>
2026-09-13 21:38:38 -0300

    feat: criacao do card de iconbutton, citycard, screen, cityfilter

--- app/(protected)/(tabs)/index.tsx
+++ app/(protected)/(tabs)/index.tsx
@@ -1,18 +1,39 @@
 import { CityCard } from "@/src/components/CityCard";
 import { Screen } from "@/src/components/Screen";
+import { CityFilter } from "@/src/containers/CityFilter";
 import { cityPreviewList } from "@/src/data/cities";
+import { useAppTheme } from "@/src/theme/useAppTheme";
 import { CityPreview } from "@/src/types";
+import { useScrollToTop } from "@react-navigation/native";
+import { useRef } from "react";
 import { FlatList, ListRenderItemInfo } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
-  // const { colors } = useAppTheme();
+  const { spacing } = useAppTheme();
+  const { top } = useSafeAreaInsets();
 
+  const flatListRef = useRef(null);
+  useScrollToTop(flatListRef);
   function renderItem({ item }: ListRenderItemInfo<CityPreview>) {
     return <CityCard cityPreview={item} />;
   }
+
   return (
     <Screen>
-      <FlatList data={cityPreviewList} renderItem={renderItem} />
+      <FlatList
+        ref={flatListRef}
+        contentContainerStyle={{
+          gap: spacing.padding,
+          paddingTop: top,
+          paddingBottom: spacing.padding,
+        }}
+        data={cityPreviewList}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        showsVerticalScrollIndicator={false}
+        ListHeaderComponent={<CityFilter />}
+      />
     </Screen>
   );
 }
---
+++ src/containers/CityFilter.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import { Box } from "../components/Box";
+import { SearchInput } from "../components/SearchInput";
+
+export function CityFilter() {
+  const [name, setName] = useState("");
+
+  return (
+    <Box>
+      <SearchInput
+        value={name}
+        onChangeText={setName}
+        placeholder="Qual seu próximo destino"
+      />
+    </Box>
+  );
+}
